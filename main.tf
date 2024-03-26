cf174c05a37adaf9182a7fb53d4a5f7f458607c1
Rajendraprasad199127 <70648283+Rajendraprasad199127@users.noreply.github.com>
2024-03-26 22:58:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,25 @@
 terraform {
   required_version = ">= 1.5.7"
+  backend "azurerm" {
+    resource_group_name  = "thomasthorntoncloud"
+    storage_account_name = "thomasthorntontfstate"
+    container_name       = "github-thomasthorntoncloud-terraform-example"
+    key                  = "github-thomasthorntoncloud-terraform-example.tfstate"
+  }
 }
- 
+
 provider "azurerm" {
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
   name     = "github-thomasthorntoncloud-terraform-example"
   location = "uksouth"
 }
- 
+
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
@@ -21,7 +27,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "uksouth"
   resource_group_name = azurerm_resource_group.tamops.name
 }
- 
+
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
