e84f8b8d59bdcd52d12bfe855004425e3ec8f4f9
Rajendraprasad199127 <70648283+Rajendraprasad199127@users.noreply.github.com>
2024-03-26 23:02:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.5.7"
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-centralindia"
-    storage_account_name = "csg10032002a93ceeea"
-    container_name       = "test"
-    key                  = "github-thomasthorntoncloud-terraform-example.tfstate"
-  }
 }
 
 provider "azurerm" {
